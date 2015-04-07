437fde43bfa401d6f18e9b7a6bb4e84dfc88b263
lzkelley <lkelley@cfa.harvard.edu>
2015-04-07 13:10:44 -0400

    Added initialization script to work as module

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+"""
+This module handles all standard plotting scripts.
+"""
+
+__author__  = "Luke Zoltan Kelley"
+__version__ = "0.1"
+__email__   = "lkelley@cfa.harvard.edu"
+__status__  = "Development"
+
+import Plotting
