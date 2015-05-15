2c4fbf895c2d13b2a3c8d40925590ab411bf5507
lzkelley <lkelley@cfa.harvard.edu>
2015-05-15 10:10:43 -0400

    Added numerous functions for numerical integration of both arrays and functions.  Also moved the method 'logSpline()' to here

--- zcode/Math.py
+++ zcode/Math.py
@@ -3,7 +3,7 @@ Functions for Math operations.
 
 Functions
 ---------
- - 
+ -
 
 
 
@@ -11,6 +11,32 @@ Functions
 
 
 import numpy as np
+import scipy as sp
+import scipy.interpolate
+
+
+def logSpline(xx, yy, order=3):
+    """
+    Create a spline interpolant in log-log-space.
+
+    Parameters
+    ----------
+    xx : array, independent variable
+    yy : array, function of ``xx``
+    order : int, order of spline interpolant
+
+    Returns
+    -------
+    spline : callable, spline interpolation function
+
+    """
+    
+    xl = np.log10(xx)
+    yl = np.log10(yy)
+    terp = sp.interpolate.InterpolatedUnivariateSpline(xl, yl, k=order)
+    spline = lambda xx: np.power(10.0, terp(np.log10(xx)))
+
+    return spline
 
 
 def contiguousInds(args):
@@ -22,7 +48,7 @@ def contiguousInds(args):
 
     # Find the indicies of changes in ``condition``
     dd = np.diff(condition)
-    idx, = dd.nonzero() 
+    idx, = dd.nonzero()
 
     # Start things after change in ``condition``, thus shift indices 1 rightward
     idx += 1
@@ -46,3 +72,111 @@ def contiguousInds(args):
     return inds
 
 # contiguousInds()
+
+
+
+
+
+def integrate_cumulative_simpson(func, xx, log=True):
+    """
+    Perform a cumulative integral of a callable functon using Simpson's rule.
+
+    Notes
+    -----
+      See: https://en.wikipedia.org/wiki/Simpson%27s_rule
+
+
+    """
+
+    left = xx[:-1]
+    rigt = xx[1:]
+    diff = np.diff(xx)
+    if( log ): cent = np.power(10.0, 0.5*(np.log10(left) + np.log10(rigt)))
+    else:      cent = 0.5*(left + rigt)
+
+    retval = func(left) + 4.0*func(cent) + func(rigt)
+    retval = np.cumsum((diff/6.0)*retval)
+    return retval
+
+# integrate_cumulative_simpson()
+
+
+
+def integrate_cumulative_func_simpson(func, xx, log=True, init=None):
+    """
+    Perform a cumulative integral of a callable functon using Simpson's rule.
+
+    Notes
+    -----
+      See: https://en.wikipedia.org/wiki/Simpson%27s_rule
+
+
+    """
+
+    left = xx[:-1]
+    rigt = xx[1:]
+    diff = np.diff(xx)
+    if( log ): cent = np.power(10.0, 0.5*(np.log10(left) + np.log10(rigt)))
+    else:      cent = 0.5*(left + rigt)
+
+    retval = func(left) + 4.0*func(cent) + func(rigt)
+    retval = np.cumsum((diff/6.0)*retval)
+    return retval
+
+# integrate_cumulative_simpson()
+
+
+
+def integrate_cumulative_arr_trapezoid(arr, xx, log=True, init=None, rev=False):
+    """
+    Perform a cumulative integral of a array using the Trapezoidal Rule
+
+    Arguments
+    ---------
+    arr  : <scalar>[N], array of values to be integrated
+    xx   : <scalar>[N], array of spacings used as the variable of integration (i.e. independent)
+    log  : (<bool>),    perform integral in log-space (i.e. average the array in log-space)
+    init : (<scalar>),  initial value of integral (i.e. constant of integration), if this value is
+                        provided (i.e. not-`None`) then it will be added to each element of the
+                        integrated values and prepended to the returned array.
+
+    Returns
+    -------
+    retval : <scalar>[*], integrated array, if ``init`` is NOT provided the length of the returned
+                          array will be [N-1] --- i.e. the number of intervals in the array, but if
+                          ``init`` IS provided, the ``init`` value will be prepended and the
+                          returned length will be [N].
+
+    Notes
+    -----
+      See: https://en.wikipedia.org/wiki/Trapezoidal_rule
+
+    """
+
+    ## Find left and right bin edges, and bin widths
+    lx = xx[:-1]
+    rx = xx[1:]
+    diff = np.diff(xx)
+
+    ## Find left and right array values, and averages
+    ly = arr[:-1]
+    ry = arr[1:]
+    # Average in log-space if desired
+    if( log ): aves = np.power(10.0, 0.5*(np.log10(ly) + np.log10(ry)))
+    else:      aves = 0.5*(ly + ry)
+
+    if( rev ): retval = np.cumsum(diff[::-1]*aves[::-1])[::-1]
+    else:      retval = np.cumsum(diff*aves)
+
+    ## If an initial value is provided, include in integral
+    if( init is not None ):
+        # Add to all values
+        retval += init
+        # Prepend as initial value (i.e. for ``xx[0]``)
+        # retval = np.insert(retval, 0, init)
+        if( rev ): retval = np.insert(retval, len(retval), init)
+        else:      retval = np.insert(retval, 0, init)
+
+    return retval
+
+# integrate_cumulative_arr_trapezoid()
