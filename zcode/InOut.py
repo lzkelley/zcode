b82364457eb46ee89e213ccacfe650378f4302c5
lzkelley <lkelley@cfa.harvard.edu>
2015-06-09 13:37:59 -0400

    Added functions 'statusString()' and 'bytesString()' originally from illpy.AuxFuncs

--- zcode/InOut.py
+++ zcode/InOut.py
@@ -4,11 +4,13 @@ Functions for Input/Output (IO) Operations.
 Functions
 ---------
 
-   getFileSize()
-   countLines()
-   checkPath()
-   dictToNPZ()
-   npzToDict()
+   statusString() :
+   bytesString()  :
+   getFileSize()  :
+   countLines()   :
+   checkPath()    :
+   dictToNPZ()    :
+   npzToDict()    :
 
 
 
@@ -17,6 +19,86 @@ Functions
 import os
 import sys
 import numpy as np
+
+
+
+def statusString(count, total, durat=None):
+    """
+    Return a description of the status and completion of an iteration.
+
+    If ``durat`` is provided it is used as the duration of time that the
+    iterations have been going.  This time is used to estimate the time
+    to completion.  ``durat`` can either be a `datetime.timedelta` object
+    of if it is a scalar (int or float) then it will be converted to
+    a `datetime.timedelta` object for string formatting purposes.
+
+    Parameters
+    ----------
+    count : int, number of iterations completed (e.g. [0...9])
+    total : int, total number of iterations (e.g. 10)
+    durat : datetime.timedelta OR scalar, (optional, default=None)
+    """
+
+    # Calculate Percentage completed
+    frac = 1.0*count/(total)
+    stat = '%.2f%%' % (100*frac)
+
+    if( durat is not None ):
+        # Make sure `durat` is a datetime.timedelta object
+        if( type(durat) is not datetime.timedelta ): durat = datetime.timedelta(seconds=durat)
+
+        # Calculate time left
+        timeLeft = 1.0*durat.total_seconds()*(1.0/frac - 1.0)
+        timeLeft = np.max([timeLeft, 0.0])
+        timeLeft = datetime.timedelta(seconds=timeLeft)
+
+        # Append to status string
+        stat += ' after %s, completion in ~ %s' % (str(durat), str(timeLeft))
+
+
+    return stat
+
+# statusString()
+
+
+def bytesString(bytes, precision=1):
+    """
+    Return a humanized string representation of a number of bytes.
+
+    Arguments
+    ---------
+    bytes : <scalar>, number of bytes
+    precision : <int>, target precision in number of decimal places
+
+    Returns
+    -------
+    strSize : <string>, human readable size
+
+    Examples
+    --------
+    >> humanize_bytes(1024*12342,2)
+    '12.05 MB'
+
+    """
+
+    abbrevs = (
+        (1<<50L, 'PB'),
+        (1<<40L, 'TB'),
+        (1<<30L, 'GB'),
+        (1<<20L, 'MB'),
+        (1<<10L, 'kB'),
+        (1, 'bytes')
+    )
+
+    for factor, suffix in abbrevs:
+        if bytes >= factor: break
+
+    # NOTE: for this to work right, must "from __future__ import division" else integer
+    strSize = '%.*f %s' % (precision, 1.0*bytes / factor, suffix)
+
+    return strSize
+
+# bytesString()
 
 
 
