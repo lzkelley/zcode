a444277e1fe302ba183859bef259cec23a4e159a
lzkelley <lkelley@cfa.harvard.edu>
2015-07-26 17:18:24 -0400

    Added class and method for creating custom, stack-based indentation logger using the logging module.  Seems to be woring well.

--- zcode/InOut.py
+++ zcode/InOut.py
@@ -5,6 +5,8 @@ Functions for Input/Output (IO) Operations.
 Classes
 -------
     StreamCapture  : class for capturing/redirecting stdout and stderr
+    IndentFormatter : <logging.Formatter> subclass for stack-depth based indentation logging
+
 
 Functions
 ---------
@@ -20,13 +22,13 @@ Functions
 
     combineFiles   :
 
+    getLogger      :
+
 """
 
-import os
-import sys
+import os, sys, logging, inspect, warnings
 import numpy as np
 
-import warnings
 
 class StreamCapture(list):
     """
@@ -66,6 +68,24 @@ class StreamCapture(list):
 
 
 # } class StreamCapture
+
+
+class IndentFormatter(logging.Formatter):
+    def __init__( self, fmt=None, datefmt=None ):
+        logging.Formatter.__init__(self, fmt, datefmt)
+        self.baseline = None
+    def format( self, rec ):
+        stack = inspect.stack()
+        if( self.baseline is None ): self.baseline = len(stack)
+        indent = (len(stack)-self.baseline)
+        rec.indent = ' -'*indent + ' '*(indent > 0)
+        out = logging.Formatter.format(self, rec)
+        del rec.indent
+        return out
+
+# } class IndentFormatter
+
+
 
 
 def statusString(count, total, durat=None):
@@ -374,3 +394,72 @@ def combineFiles(inFilenames, outFilename, verbose=False):
     return
 
 # combineFiles()
+
+
+def getLogger(name, strFmt=None, fileFmt=None, dateFmt=None, strLevel=None, fileLevel=None,
+              tofile=None, tostr=True):
+    """
+    Create a standard logger object which logs to file and or stdout stream ('str')
+
+    Arguments
+    ---------
+        name    <str> : handle for this logger, must be distinct for a distinct logger
+
+        strFmt  <str>  : format of log messages to stream (stdout)
+        fileFmt <str>  : format of log messages to file
+        dateFmt <str>  : format of time stamps to stream and/or file
+        strLevel  <int>  : logging level for stream
+        fileLevel <int>  : logging level for file
+        tofile  <str>  : filename to log to (turned off if `None`)
+        tostr   <bool> : log to stdout stream
+
+    Returns
+    -------
+        logger  <obj>  : ``logging`` logger object
+
+    """
+
+    if( tofile is None and not tostr ): raise RuntimeError("Must log to something")
+
+    logger = logging.getLogger(name)
+    # Make sure handlers don't get duplicated (ipython issue)
+    while len(logger.handlers) > 0: logger.handlers.pop()
+
+    ## Determine and Set Logging Level
+    if( fileLevel is None ): fileLevel = logging.WARNING
+    if( strLevel  is None ): strLevel  = logging.WARNING
+    # Logger object must be at minimum level
+    logger.setLevel(np.min([fileLevel, strLevel]))
+
+    if( dateFmt is None ): dateFmt = '%Y/%m/%d %H:%M:%S'
+
+    ## Log to file
+    #  -----------
+    if( tofile is not None ):
+        if( fileFmt is None ):
+            fileFmt  = "%(asctime)s %(levelname)8.8s [%(filename)15.15s:"
+            fileFmt += "%(funcName)-15.15s]%(indent)s%(message)s"
+
+        fileFormatter = IndentFormatter(fileFmt, datefmt=dateFmt)
+        fileHandler = logging.FileHandler(tofile, 'w')
+        fileHandler.setFormatter(fileFormatter)
+        fileHandler.setLevel(fileLevel)
+        logger.addHandler(fileHandler)
+
+
+    ## Log To stdout
+    #  -------------
+    if( tostr ):
+        if( strFmt is None ):
+            strFmt = "%(indent)s %(message)s"
+
+        strFormatter = IndentFormatter(strFmt, datefmt=dateFmt)
+        strHandler = logging.StreamHandler()
+        strHandler.setFormatter(strFormatter)
+        strHandler.setLevel(strLevel)
+        logger.addHandler(strHandler)
+
+
+    return logger
+
+# getLogger()
