3a1aa91543461b7cea808ced84c14ba82abb1c35
lzkelley <lkelley@cfa.harvard.edu>
2015-10-19 18:30:47 -0400

    Minor formatting changs

--- zcode/__init__.py
+++ zcode/__init__.py
@@ -2,14 +2,12 @@
 
 """
 
-import plotting
+from . import plotting
 
-__author__  = "Luke Zoltan Kelley"
+__author__ = "Luke Zoltan Kelley"
 __version__ = "0.1"
-__email__   = "lkelley@cfa.harvard.edu"
-__status__  = "Development"
-
+__email__ = "lkelley@cfa.harvard.edu"
+__status__ = "Development"
 
 __all__ = []
 __all__.extend(plotting.__all__)
-
