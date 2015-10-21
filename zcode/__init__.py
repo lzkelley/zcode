0420832b630d24cb8aace31a06c8b13134b2a90a
lzkelley <lkelley@cfa.harvard.edu>
2015-10-21 14:52:38 -0400

    Moved 'Constants.py' to 'constants.py' --- added to '__init__.py'

--- zcode/__init__.py
+++ zcode/__init__.py
@@ -5,6 +5,7 @@
 from . import plot
 from . import math
 from . import inout
+from . import constants
 
 __author__ = "Luke Zoltan Kelley"
 __version__ = "0.1"
@@ -12,6 +13,3 @@ __email__ = "lkelley@cfa.harvard.edu"
 __status__ = "Development"
 
 __all__ = ['__version__', '__author__', '__email__', '__status__']
-# __all__.extend(plot.__all__)
-# __all__.extend(math.__all__)
-# __all__.extend(inout.__all__)
