06a780179541fb0ffc96fa3fbff31c4230d31c54
lzkelley <lkelley@cfa.harvard.edu>
2015-10-22 18:24:02 -0400

    Added missing 'CorrelationGrid' import

--- zcode/plot/__init__.py
+++ zcode/plot/__init__.py
@@ -1,11 +1,14 @@
 """Plotting methods.
 """
 
-from . import Hist2D
-from Hist2D import *
 from . import plot_core
 from plot_core import *
+from . import Hist2D
+from Hist2D import *
+from . import CorrelationGrid
+from CorrelationGrid import *
 
 __all__ = []
 __all__.extend(plot_core.__all__)
 __all__.extend(Hist2D.__all__)
+__all__.extend(CorrelationGrid.__all__)
