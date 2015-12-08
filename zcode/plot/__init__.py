3ab0630941dcac45e79d289838d597e4aacd4f59
lzkelley <lkelley@cfa.harvard.edu>
2015-12-08 15:40:39 -0500

    ENH: New-file 'color2d.py' for producing 2d colormaps.

--- zcode/plot/__init__.py
+++ zcode/plot/__init__.py
@@ -7,8 +7,11 @@ from . import Hist2D
 from .Hist2D import *
 from . import CorrelationGrid
 from .CorrelationGrid import *
+from . import color2d
+from .color2d import *
 
 __all__ = []
 __all__.extend(plot_core.__all__)
 __all__.extend(Hist2D.__all__)
 __all__.extend(CorrelationGrid.__all__)
+__all__.extend(color2d.__all__)
---
+++ zcode/plot/color2d.py
@@ -0,0 +1,18 @@
+"""Providing two-dimensional colormaps.
+
+Classes
+-------
+
+Functions
+---------
+
+"""
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import zcode.math as zmath
+from . import plot_core as zplot
+
+__all__ = []
