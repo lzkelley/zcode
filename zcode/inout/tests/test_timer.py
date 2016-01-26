91f4868c41b469b1eb68d29fb841b512c18222fc
lzkelley <lkelley@cfa.harvard.edu>
2016-01-26 14:21:21 -0500

    ENH: added actual testing stuff to show working of timer.

--- zcode/inout/tests/test_timer.py
+++ zcode/inout/tests/test_timer.py
@@ -5,6 +5,7 @@ Can be run with:
 
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
+from six.moves import xrange
 
 import os
 import warnings
@@ -18,6 +19,7 @@ class TestTimer(object):
 
     @classmethod
     def setup_class(cls):
+        cls.NUM_ITER = 10
         pass
 
     @classmethod
@@ -27,8 +29,46 @@ class TestTimer(object):
     def test_dictToNPZ_npzToDict(self):
         from zcode.inout.timer import Timer
 
+        # Create `Timer` object
         times = Timer()
 
+        for ii in xrange(self.NUM_ITER):
+            times.start('one')
+            np.random.randint(-1000, 1000, size=1000000)
+            times.stop('one')
+
+            times.start('two')
+            NUM = 200
+            ss = np.arange(3, NUM+1, 2)
+            mroot = NUM ** 0.5
+            half = (NUM + 1)//2 - 1
+            ii = 0
+            mm = 3
+            while mm <= mroot:
+                if ss[ii]:
+                    jj = (mm * mm - 3)/2
+                    ss[jj] = 0
+                    while jj < half:
+                        ss[jj] = 0
+                        jj += mm
+                ii += 1
+                mm = 2*ii + 3
+
+            times.stop('two')
+
+            times.start('three')
+            np.sort(np.random.permutation(np.arange(1000000)))
+            times.stop('three')
+
+        for ii in xrange(len(times)):
+            print(times.names[ii])
+            for jj in times.durations[ii]:
+                print(jj, end=' ')
+            print("\n")
+
+        print("Averages = ", times.average())
+
+        # raise RuntimeError()
 
 # Run all methods as if with `nosetests ...`
 if __name__ == "__main__":
