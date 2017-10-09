f9391f9cfa54fa82925f252043c51247c5e81d06
lzkelley <lkelley@cfa.harvard.edu>
2017-10-09 10:53:55 -0400

    MAINT: formatting

--- zcode/math/math_core.py
+++ zcode/math/math_core.py
@@ -508,17 +508,15 @@ def minmax(data, prev=None, stretch=None, log_stretch=None, filter=None, limit=N
     if limit is not None:
         assert len(limit) == 2, "`limit` must have length 2."
 
-    _data = np.array(data)
     if filter:
-        _data = comparison_filter(_data, filter)
+        data = comparison_filter(data, filter)
 
     # If there are no elements (left), return `prev` (`None` if not provided)
-    if np.size(_data) == 0:
+    if np.size(data) == 0:
         return prev
 
     # Find extrema
-    # minmax = np.array([lef*np.min(_data), rit*np.max(_data)])
-    minmax = np.array([np.min(_data), np.max(_data)])
+    minmax = np.array([np.min(data), np.max(data)])
 
     # Add stretch (relative to center point)
     if (stretch is not None) or (log_stretch is not None):
@@ -531,13 +529,17 @@ def minmax(data, prev=None, stretch=None, log_stretch=None, filter=None, limit=N
 
     # Compare to previous extrema, if given
     if prev is not None:
-        if prev[0] is not None: minmax[0] = np.min([minmax[0], prev[0]])
-        if prev[1] is not None: minmax[1] = np.max([minmax[1], prev[1]])
+        if prev[0] is not None:
+            minmax[0] = np.min([minmax[0], prev[0]])
+        if prev[1] is not None:
+            minmax[1] = np.max([minmax[1], prev[1]])
 
     # Compare to limits, if given
     if limit is not None:
-        if limit[0] is not None: minmax[0] = np.max([minmax[0], limit[0]])
-        if limit[1] is not None: minmax[1] = np.min([minmax[1], limit[1]])
+        if limit[0] is not None:
+            minmax[0] = np.max([minmax[0], limit[0]])
+        if limit[1] is not None:
+            minmax[1] = np.min([minmax[1], limit[1]])
 
     # Round the min/max results to given number of sig-figs
     if round is not None:
