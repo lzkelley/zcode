22c23c8cdafeef71a842662221caed6e03a73895
lzkelley <lkelley@cfa.harvard.edu>
2018-09-12 15:11:22 -0400

    Update setup script and version tracking

--- zcode/__init__.py
+++ zcode/__init__.py
@@ -3,17 +3,13 @@
 """
 # flake8: noqa  --- ignore imported but unused flake8 warnings
 
-# from . import plot
-# from . import math
-# from . import inout
 from . import constants
 
+with open('VERSION') as inn:
+    version = inn.read().strip()
+
 __author__ = "Luke Zoltan Kelley"
-__version__ = "0.0.12"
+__version__ = version
 __email__ = "lkelley@cfa.harvard.edu"
 __status__ = "Development"
 
-__all__ = ['__version__', '__author__', '__email__', '__status__']
-
-# from numpy.testing import Tester
-# test = Tester().test
