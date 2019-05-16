696e904adf989f28ecbd2e015ae09cd3adae0fa0
lzkelley <lkelley@cfa.harvard.edu>
2019-05-16 11:36:05 -0500

    Allow bandwidths to be selected per-variable; allow particular variables to be preserved during resampling

--- zcode/math/kde.py
+++ zcode/math/kde.py
@@ -11,24 +11,22 @@ import numpy as np
 
 import zcode.math as zmath
 
-__all__ = ['gaussian_kde']
+__all__ = ['KDE']
 
 
-class gaussian_kde(object):
+class KDE(object):
     """
     """
-    def __init__(self, dataset, bw_scale=None, bw_method=None, weights=None, neff=None, warn=True):
+    bw_method_default = 'scott'
+
+    def __init__(self, dataset, bw_method=None, weights=None, neff=None):
         self.dataset = np.atleast_2d(dataset)
         if not self.dataset.size > 1:
             raise ValueError("`dataset` input should have multiple elements.")
 
         self.ndims, self.nvals = self.dataset.shape
-        if bw_scale is None:
-            bw_scale = 1.0
-
-        self.bw_scale = bw_scale
-        if warn and (not np.isclose(bw_scale, 1.0)):
-            logging.warning("WARNING: rescaling the bandwidth by `{:.4e}`".format(bw_scale))
+        if weights is None:
+            weights = np.ones(self.nvals)/self.nvals
 
         if weights is not None:
             if np.count_nonzero(weights) == 0 or np.any(~np.isfinite(weights) | (weights < 0)):
@@ -41,33 +39,13 @@ class gaussian_kde(object):
             if len(self._weights) != self.nvals:
                 raise ValueError("`weights` input should be of length n")
 
-            if neff is None:
-                neff = 1/np.sum(self._weights**2)
-
         self._neff = neff
+        self._weights = weights
+        self._compute_covariance()
         self.set_bandwidth(bw_method=bw_method)
         return
 
-    def evaluate(self, points):
-        """Evaluate the estimated pdf on a set of points.
-
-        Parameters
-        ----------
-        points : (# of dimensions, # of points)-array
-            Alternatively, a (# of dimensions,) vector can be passed in and
-            treated as a single point.
-
-        Returns
-        -------
-        values : (# of points,)-array
-            The values at each point.
-
-        Raises
-        ------
-        ValueError : if the dimensionality of the input points is different than
-                     the dimensionality of the KDE.
-
-        """
+    def pdf(self, points):
         points = np.atleast_2d(points)
 
         ndim, nv = points.shape
@@ -85,7 +63,7 @@ class gaussian_kde(object):
         # Compute Cholesky matrix from the covariance matrix, this matrix will transform the
         # dataset to one with a diagonal covariance matrix (i.e. independent variables)
         # note: `sp.linalg.cholesky` uses an *upper* matrix by default, and numpy uses *lower*
-        whitening = sp.linalg.cholesky(self.inv_cov)
+        whitening = sp.linalg.cholesky(self.bw_cov_inv)
         # Construct the 'whitened' (independent) dataset
         scaled_dataset = np.dot(whitening, self.dataset)
         # Construct the whitened sampling points
@@ -107,71 +85,28 @@ class gaussian_kde(object):
                 energy = np.sum(diff * diff, axis=0) / 2.0
                 result[i] = np.sum(np.exp(-energy)*self.weights, axis=0)
 
-        result = result / self._norm_factor
+        result = result / self.bw_norm
 
         return result
 
-    def integrate_kde(self, other):
-        """
-        Computes the integral of the product of this  kernel density estimate
-        with another.
-
-        Parameters
-        ----------
-        other : gaussian_kde instance
-            The other kde.
-
-        Returns
-        -------
-        value : scalar
-            The result of the integral.
-
-        Raises
-        ------
-        ValueError
-            If the KDEs have different dimensionality.
-
-        """
-        if other.d != self.ndims:
-            raise ValueError("KDEs are not the same dimensionality")
-
-        # we want to iterate over the smallest number of points
-        if other.n < self.nvals:
-            small = other
-            large = self
-        else:
-            small = self
-            large = other
-
-        sum_cov = small.covariance + large.covariance
-        sum_cov_chol = np.linalg.cho_factor(sum_cov)
-        result = 0.0
-        for i in range(small.n):
-            mean = small.dataset[:, i, np.newaxis]
-            diff = large.dataset - mean
-            tdiff = np.linalg.cho_solve(sum_cov_chol, diff)
-
-            energies = np.sum(diff * tdiff, axis=0) / 2.0
-            result += np.sum(np.exp(-energies)*large.weights, axis=0)*small.weights[i]
-
-        sqrt_det = np.prod(np.diagonal(sum_cov_chol[0]))
-        norm_const = np.power(2 * np.pi, sum_cov.shape[0] / 2.0) * sqrt_det
-
-        result /= norm_const
-
-        return result
-
-    def resample(self, size=None):
+    def resample(self, size=None, keep=None):
         if size is None:
             size = int(self.neff)
+
+        bw_cov = np.array(self.bw_cov)
+        if keep is not None:
+            keep = np.atleast_1d(keep)
+            for pp in keep:
+                bw_cov[pp, :] = 0.0
+                bw_cov[:, pp] = 0.0
 
         # Draw from the smoothing kernel
         # here the `cov` includes the bandwidth
         try:
-            norm = np.random.multivariate_normal(np.zeros(self.ndims), self.cov, size=size)
+            norm = np.random.multivariate_normal(np.zeros(self.ndims), bw_cov, size=size)
         except ValueError as err:
             logging.error("Failed to construct `multivariate_normal`!  " + str(err))
-            logging.error("cov = '{}'".format(self.cov))
+            logging.error("cov = '{}'".format(self.bw_cov))
             logging.error("dataset = '{}'".format(zmath.stats_str(self.dataset)))
             raise
 
@@ -184,123 +119,98 @@ class gaussian_kde(object):
 
         return samps
 
-    def scotts_factor(self):
-        """Compute Scott's factor.
-
-        Returns
-        -------
-        s : float
-            Scott's factor.
-        """
+    def scott_factor(self, *args, **kwargs):
         return np.power(self.neff, -1./(self.ndims+4))
 
-    def silverman_factor(self):
-        """Compute the Silverman factor.
-
-        Returns
-        -------
-        s : float
-            The silverman factor.
-        """
+    def silverman_factor(self, *args, **kwargs):
         return np.power(self.neff*(self.ndims+2.0)/4.0, -1./(self.ndims+4))
 
     def set_bandwidth(self, bw_method=None):
-        if bw_method is None:
-            self.bandwidth_func = self.scotts_factor
-        elif bw_method == 'scott':
-            self.bandwidth_func = self.scotts_factor
-        elif bw_method == 'silverman':
-            self.bandwidth_func = self.silverman_factor
-        elif np.isscalar(bw_method) and not isinstance(bw_method, six.string_types):
-            self._bw_method = 'use constant'
-            self.bandwidth_func = lambda: bw_method
-        elif callable(bw_method):
-            self._bw_method = bw_method
-            self.bandwidth_func = lambda: self._bw_method(self)
-        else:
-            msg = "`bw_method` should be 'scott', 'silverman', a scalar " \
-                  "or a callable."
-            raise ValueError(msg)
+        ndims = self.ndims
+        _bw_method = bw_method
+        bw_white = np.zeros((ndims, ndims))
 
-        self._compute_covariance()
+        if len(np.atleast_1d(bw_method)) == 1:
+            _bw, bw_method = self._compute_bandwidth(bw_method)
+            bw_white[...] = _bw
+        else:
+            if np.shape(bw_method) == (ndims,):
+                # bw_method = 'diagonal'
+                for ii in range(self.ndims):
+                    bw_white[ii, ii] = self._compute_bandwidth(
+                        bw_method[ii], param=(ii, ii))[0]
+                bw_method = 'diagonal'
+            elif np.shape(bw_method) == (ndims, ndims):
+                for ii, jj in np.ndindex(ndims, ndims):
+                    bw_white[ii, jj] = self._compute_bandwidth(
+                        bw_method[ii, jj], param=(ii, jj))[0]
+                bw_method = 'matrix'
+            else:
+                raise ValueError("`bw_method` have shape (1,), (N,) or (N,) for `N` dimensions!")
+
+        bw_cov = self._data_cov * (bw_white ** 2)
+        try:
+            bw_cov_inv = np.linalg.inv(bw_cov)
+        except np.linalg.LinAlgError:
+            logging.warning("WARNING: singular `bw_cov` matrix, trying SVD...")
+            bw_cov_inv = np.linalg.pinv(bw_cov)
+
+        self.bw_white = bw_white
+        self.bw_method = bw_method
+        self._bw_method = _bw_method
+        self.bw_cov = bw_cov
+        self.bw_cov_inv = bw_cov_inv
+        self.bw_norm = np.sqrt(np.linalg.det(2*np.pi*self.bw_cov))
         return
+
+    def _compute_bandwidth(self, bw_method, param=None):
+        if bw_method is None:
+            bw_method = self.bw_method_default
+
+        if isinstance(bw_method, six.string_types):
+            if bw_method == 'scott':
+                bandwidth = self.scott_factor(param=param)
+            elif bw_method == 'silverman':
+                bandwidth = self.silverman_factor(param=param)
+            else:
+                msg = "Unrecognized bandwidth str specification '{}'!".format(bw_method)
+                raise ValueError(msg)
+
+        elif np.isscalar(bw_method):
+            if np.isclose(bw_method, 0.0):
+                msg = "`bw_method` '{}' for param '{}' cannot be zero!".format(bw_method, param)
+                raise ValueError(msg)
+
+            bandwidth = bw_method
+            bw_method = 'constant scalar'
+
+        elif callable(bw_method):
+            bw_method = bw_method
+            bandwidth = bw_method(self, param=param)
+
+        else:
+            raise ValueError("Unrecognized `bw_method` '{}'!".format(bw_method))
+
+        return bandwidth, bw_method
 
     def _compute_covariance(self):
         """Computes the covariance matrix for each Gaussian kernel using bandwidth_func().
         """
 
-        # This is the bandwidth for whitened data (i.e. sigma_i = 1 for all i)
-        #  sqrt(H)
-        bandwidth_white = self.bandwidth_func()
-        # print("{:.4e} {:.4e}".format(bandwidth_white, self.dataset.size))
-        bandwidth_white *= self.bw_scale
         # Cache covariance and inverse covariance of the data
         if not hasattr(self, '_data_cov'):
             cov = np.cov(self.dataset, rowvar=True, bias=False, aweights=self.weights)
             self._data_cov = np.atleast_2d(cov)
-        if not hasattr(self, '_data_inv_cov'):
-            self._data_inv_cov = np.linalg.inv(self._data_cov)
+        # if not hasattr(self, '_data_inv_cov'):
+        #     self._data_inv_cov = np.linalg.inv(self._data_cov)
 
-        HH = bandwidth_white**2
-        # This is the smoothing-kernel matrix H_ij, for the full data
-        self.cov = self._data_cov * HH
-        self.inv_cov = self._data_inv_cov / HH
-        # Including `2pi` in the determinant takes it to the d'th power.
-        self._norm_factor = np.sqrt(np.linalg.det(2*np.pi*self.cov))
-        self.bandwidth_white = bandwidth_white
         return
-
-    def pdf(self, x):
-        """
-        Evaluate the estimated pdf on a provided set of points.
-
-        Notes
-        -----
-        This is an alias for `gaussian_kde.evaluate`.  See the ``evaluate``
-        docstring for more details.
-
-        """
-        return self.evaluate(x)
-
-    def logpdf(self, x):
-        """
-        Evaluate the log of the estimated pdf on a provided set of points.
-        """
-
-        points = np.atleast_2d(x)
-
-        d, m = points.shape
-        if d != self.ndims:
-            if d == 1 and m == self.ndims:
-                # points was passed in as a row vector
-                points = np.reshape(points, (self.ndims, 1))
-                m = 1
-            else:
-                msg = "points have dimension %s, dataset has dimension %s" % (d, self.ndims)
-                raise ValueError(msg)
-
-        if m >= self.nvals:
-            # there are more points than data, so loop over data
-            energy = np.zeros((self.nvals, m), dtype=float)
-            for i in range(self.nvals):
-                diff = self.dataset[:, i, np.newaxis] - points
-                tdiff = np.dot(self.inv_cov, diff)
-                energy[i] = np.sum(diff*tdiff, axis=0) / 2.0
-            result = sp.special.logsumexp(-energy.T, b=self.weights / self._norm_factor, axis=1)
-        else:
-            # loop over points
-            result = np.zeros((m,), dtype=float)
-            for i in range(m):
-                diff = self.dataset - points[:, i, np.newaxis]
-                tdiff = np.dot(self.inv_cov, diff)
-                energy = np.sum(diff * tdiff, axis=0) / 2.0
-                result[i] = sp.special.logsumexp(-energy, b=self.weights / self._norm_factor)
-
-        return result
 
     @property
     def weights(self):
         try:
+            if self._weights is None:
+                raise AttributeError
             return self._weights
         except AttributeError:
             self._weights = np.ones(self.nvals)/self.nvals
@@ -308,9 +218,10 @@ class gaussian_kde(object):
 
     @property
     def neff(self):
-        neff = self._neff
-        if neff is None:
-            neff = 1/np.sum(self.weights**2)
-            self._neff = neff
-
-        return neff
+        try:
+            if self._neff is None:
+                raise AttributeError
+            return self._neff
+        except AttributeError:
+            self._neff = 1.0 / np.sum(self.weights**2)
+            return self._neff
--- zcode/math/tests/test_kde.py
+++ zcode/math/tests/test_kde.py
@@ -9,7 +9,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import numpy as np
 import scipy as sp
 from numpy.testing import run_module_suite
-from nose.tools import assert_true, assert_false
+from nose.tools import assert_true
 
 import zcode.math as zmath
 
@@ -31,7 +31,7 @@ class Test_KDE(object):
         grid = zmath.spacing(bins, 'lin', 3000)
 
         methods = ['scott', 0.04, 0.2, 0.8]
-        classes = [sp.stats.gaussian_kde, zmath.kde.gaussian_kde]
+        classes = [sp.stats.gaussian_kde, zmath.kde.KDE]
         for mm in methods:
             kdes = [cc(aa, bw_method=mm).pdf(grid) for cc in classes]
             print("bw_method='{}'".format(mm))
@@ -40,7 +40,7 @@ class Test_KDE(object):
         return
 
     def test_compare_scipy_2d(self):
-        print("\Test_KDE:test_compare_scipy_2d()")
+        print("\n|Test_KDE:test_compare_scipy_2d()|")
 
         NUM = 1000
         a1 = np.random.normal(6.0, 1.0, NUM//2)
@@ -58,13 +58,136 @@ class Test_KDE(object):
         grid = np.vstack([xc.ravel(), yc.ravel()])
 
         methods = ['scott', 0.04, 0.2, 0.8]
-        classes = [sp.stats.gaussian_kde, zmath.kde.gaussian_kde]
+        classes = [sp.stats.gaussian_kde, zmath.kde.KDE]
         for mm in methods:
             print("bw_method='{}'".format(mm))
             kdes = [cc(data, bw_method=mm).pdf(grid).reshape(xc.shape).T for cc in classes]
             assert_true(np.allclose(kdes[0], kdes[1]))
 
         return
+
+    def test_different_bws(self):
+        print("\n|Test_KDE:test_different_bws()|")
+        np.random.seed(9235)
+        NUM = 1000
+        a1 = np.random.normal(6.0, 1.0, NUM//2)
+        a2 = np.random.lognormal(0, 0.5, size=NUM//2)
+        aa = np.concatenate([a1, a2])
+
+        bb = np.random.normal(3.0, 0.02, NUM) + aa/100
+
+        data = [aa, bb]
+        edges = [zmath.spacing(dd, 'lin', 100, stretch=1.0) for dd in data]
+        cents = [zmath.midpoints(ee, 'lin') for ee in edges]
+
+        xe, ye = np.meshgrid(*edges)
+        xc, yc = np.meshgrid(*cents)
+
+        bws = [0.5, 2.0]
+        kde2d = zmath.kde.KDE(data, bw_method=bws)
+        kde1d = [zmath.kde.KDE(dd, bw_method=ss) for dd, ss in zip(data, bws)]
+
+        for ii in range(2):
+            samp_1d = kde1d[ii].resample(NUM).squeeze()
+            samp_2d = kde2d.resample(NUM)[ii]
+
+            # Make sure the two distributions resemble eachother
+            ks, pv = sp.stats.ks_2samp(samp_1d, samp_2d)
+            # Calibrated to the above seed-value of `9235`
+            assert_true(pv > 0.5)
+
+        return
+
+    def test_resample_keep_params_1(self):
+        print("\n|Test_KDE:test_resample_keep_params_1()|")
+        np.random.seed(9235)
+        NUM = 1000
+
+        # Construct some random data
+        # ------------------------------------
+        a1 = np.random.normal(6.0, 1.0, NUM//2)
+        a2 = np.random.lognormal(1.0, 0.5, size=NUM//2)
+        aa = np.concatenate([a1, a2])
+
+        bb = np.random.normal(3.0, 0.02, NUM) + aa/100
+
+        data = [aa, bb]
+
+        norm = 2.3
+
+        # Add an array of uniform values at location `ii`, make sure they are preserved in resample
+        for ii in range(3):
+            test = np.array(data)
+            test = np.insert(test, ii, norm*np.ones_like(test[0]), axis=0)
+
+            # Construct KDE
+            kde3d = zmath.kde.KDE(test)
+
+            # Resample from KDE preserving the uniform data
+            samples = kde3d.resample(NUM, keep=ii)
+            # Make sure the uniform values are still the same
+            param_samp = samples[ii]
+            assert_true(np.allclose(param_samp, norm))
+
+            # Make sure the other two parameters are consistent (KS-test) with input data
+            samples = np.delete(samples, ii, axis=0)
+            for jj in range(2):
+                stuff = [samples[jj], data[jj]]
+                ks, pv = sp.stats.ks_2samp(*stuff)
+                # msg = "{} {} :: {:.2e} {:.2e}".format(ii, jj, ks, pv)
+                # print(msg)
+                assert_true(pv > 0.2)
+
+        return
+
+    def test_resample_keep_params_2(self):
+        print("\n|Test_KDE:test_resample_keep_params_2()|")
+
+        # Construct random data
+        # -------------------------------
+        np.random.seed(2235)
+        NUM = 300
+        a1 = np.random.normal(6.0, 1.0, NUM//2)
+        a2 = np.random.lognormal(1.0, 0.5, size=NUM//2)
+        aa = np.concatenate([a1, a2])
+
+        bb = np.random.normal(3.0, 0.02, NUM) + aa/100
+
+        data = [aa, bb]
+
+        norms = [2.3, -3.4]
+        # Choose two locations to insert new, uniform variables
+        for ii in range(3):
+            jj = ii
+            # Make sure the locations are different
+            while jj == ii:
+                jj = np.random.choice(3)
+
+            # Insert uniform arrays
+            lo = np.min([ii, jj])
+            hi = np.max([ii, jj])
+            test = np.array(data)
+            test = np.insert(test, lo, norms[0]*np.ones_like(test[0]), axis=0)
+            test = np.insert(test, hi, norms[1]*np.ones_like(test[0]), axis=0)
+
+            # Construct KDE and draw new samples preserving the inserted variables
+            kde4d = zmath.kde.KDE(test)
+            samples = kde4d.resample(NUM, keep=(lo, hi))
+            # Make sure the target variables are preserved
+            for kk, ll in enumerate([lo, hi]):
+                param_samps = samples[ll]
+                # print(norms[kk], zmath.stats_str(param_samps))
+                assert_true(np.allclose(param_samps, norms[kk]))
+
+            # Make sure the resamples data is all consistent with input
+            for jj in range(4):
+                stuff = [samples[jj], test[jj]]
+                ks, pv = sp.stats.ks_2samp(*stuff)
+                # msg = "{} {} :: {:.2e} {:.2e}".format(ii, jj, ks, pv)
+                assert_true(pv > 0.1)
+
+        return
+
 
 
 # Run all methods as if with `nosetests ...`
