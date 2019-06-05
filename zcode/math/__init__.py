250633102a4c23c92c6932d8a49c9534360f02b1
lzkelley <lkelley@cfa.harvard.edu>
2019-06-05 10:53:35 -0500

    BUG: remove deprecated kde import

--- zcode/math/__init__.py
+++ zcode/math/__init__.py
@@ -9,15 +9,12 @@ from . import numeric
 from .numeric import *
 from . import statistic
 from .statistic import *
-from . import kde
-from .kde import *
 
 __all__ = []
 __all__.extend(math_core.__all__)
 __all__.extend(hist.__all__)
 __all__.extend(statistic.__all__)
 __all__.extend(numeric.__all__)
-__all__.extend(kde.__all__)
 
 from numpy.testing import Tester
 test = Tester().test
