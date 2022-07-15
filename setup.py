c09960b39aa80ed4f487c7f154a2f8ece542f61d
Luke Zoltan Kelley <lzkelley@gmail.com>
2022-07-15 13:29:04 -0500

    Update boilerplate stuff

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
-from setuptools import setup
+"""Utility functions and tools from Luke Zoltan Kelley.
+"""
+
+from setuptools import setup, find_packages
+
+short_description = __doc__.strip()
 
 with open('requirements.txt') as inn:
     requirements = inn.read().splitlines()
@@ -11,22 +16,24 @@ with open('zcode/VERSION') as inn:
 
 setup(
     name="zcode",
-    version=version,
     author="Luke Zoltan Kelley",
     author_email="lzkelley@gmail.com",
-    description=("General, commonly used functions for other projects."),
-    license="MIT",
-    keywords="",
-    url="https://bitbucket.org/lzkelley/zcode/",
-    packages=['zcode'],
-    include_package_data=True,
-    install_requires=requirements,
+    url="https://github.com/lzkelley/zcode/",
+    version=version,
+    description=short_description,
     long_description=long_description,
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 2.7',
-    ],
+    long_description_content_type="text/markdown",
+    license="MIT",
+    # External dependencies loaded from 'requirements.txt'
+    install_requires=requirements,
+    # Which Python importable modules should be included when your package is installed
+    # Handled automatically by setuptools. Use 'exclude' to prevent some specific
+    # subpackage(s) from being added, if needed
+    packages=find_packages(),
+    include_package_data=True,
+    # Optional include package data to ship with your package
+    # Customize MANIFEST.in if the general case does not suit your needs
+    # Comment out this line to prevent the files from being packaged with your software
+    include_package_data=True,
+    python_requires=">=3.9",          # Python version restrictions
 )
