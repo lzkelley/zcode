f0ed4959afa0cbee9af9e0fa4f048b9d6e5942dd
Luke Zoltan Kelley <lzkelley@gmail.com>
2022-09-23 10:48:48 -0600

    Add 'autoscale' functionality to 'zcode.plot.layout'

--- zcode/plot/layout.py
+++ zcode/plot/layout.py
@@ -10,8 +10,40 @@ import six
 from zcode.plot import _PAD
 
 
-__all__ = ["backdrop", "extent", "full_extent", "position_to_extent", "rect_for_inset",
+__all__ = ["autoscale", "backdrop", "extent", "full_extent", "position_to_extent", "rect_for_inset",
            "transform"]
+
+
+def autoscale(ax=None, axis='y', margin=0.1):
+    """Autoscale the given axis to match manually set bounds on the other axis.
+
+    From @thomasaarholt
+    https://gist.github.com/thomasaarholt/c8440b132aaea9f71f0588af486ad457
+    """
+
+    if ax is None:
+        ax = plt.gca()
+    newlow, newhigh = np.inf, -np.inf
+
+    for artist in ax.collections + ax.lines:
+        x, y = _autoscale__get_xy(artist)
+        if axis == 'y':
+            setlim = ax.set_ylim
+            lim = ax.get_xlim()
+            fixed, dependent = x, y
+        else:
+            setlim = ax.set_xlim
+            lim = ax.get_ylim()
+            fixed, dependent = y, x
+
+        low, high = _autoscale__calculate_new_limit(fixed, dependent, lim)
+        newlow = low if low < newlow else newlow
+        newhigh = high if high > newhigh else newhigh
+
+    margin = margin * (newhigh - newlow)
+
+    setlim(newlow-margin, newhigh+margin)
+    return
 
 
 def backdrop(fig, obj, pad=0.0, union=False, group=False, draw=True, **kwargs):
@@ -319,3 +351,39 @@ def _set_extents(items, pad=0.0, union=False, **kw):
     if union:
         bboxes = mpl.transforms.Bbox.union(bboxes)
     return bboxes
+
+
+def _autoscale__calculate_new_limit(fixed, dependent, limit):
+    """Calculates the min/max of the dependent axis given a fixed axis with limits
+
+    From @thomasaarholt
+    https://gist.github.com/thomasaarholt/c8440b132aaea9f71f0588af486ad457
+    """
+    if len(fixed) > 2:
+        mask = (fixed > limit[0]) & (fixed < limit[1])
+        window = dependent[mask]
+        low, high = window.min(), window.max()
+    else:
+        low = dependent[0]
+        high = dependent[-1]
+        if low == 0.0 and high == 1.0:
+            # This is a axhline in the autoscale direction
+            low = np.inf
+            high = -np.inf
+    return low, high
+
+
+def _autoscale__get_xy(artist):
+    """Get the xy coordinates of a given artist.
+
+    From @thomasaarholt
+    https://gist.github.com/thomasaarholt/c8440b132aaea9f71f0588af486ad457
+    """
+    if "Collection" in str(artist):
+        x, y = artist.get_offsets().T
+    elif "Line" in str(artist):
+        x, y = artist.get_xdata(), artist.get_ydata()
+    else:
+        raise ValueError("This type of object isn't implemented yet")
+
+    return x, y
